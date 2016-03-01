977c4dcff6bffb8462d76a9a16b525b549b3b806
PaulBrimley <paul.k.brimley@gmail.com>
2016-03-01 11:46:32 -0700

    sideBar team menu work

--- core/app/containers/sideBar.jsx
+++ core/app/containers/sideBar.jsx
@@ -48,8 +48,15 @@ class SideBar extends Component{
 					
 					<div className="activeTeamName">
 						<span className="activeTeamHeader"> <Link to="/home">{team.teamName} </Link> </span>
-
-						<Link to="/team/invite" className="zmdi zmdi-menu zmdi-hc-2x menuIcon"></Link>
+						<div className="dropDownMenu">
+							<i className="zmdi zmdi-menu zmdi-hc-2x" style={{fontSize: '1.4em'}}></i>
+							<div className="dropDownContent">
+								<div className="menuIcon"><span className="menuIconInfo" style={{bottom: '2px'}}>Manage Members</span><Link to="/team/invite" className="zmdi zmdi-account zmdi-hc-2x" style={{fontSize: '2.2em'}}></Link></div>
+								<div className="menuIcon"><span className="menuIconInfo">Manage Team</span><Link to="/team/invite" className="zmdi zmdi-edit zmdi-hc-2x"></Link></div>
+								<div className="menuIcon"><span className="menuIconInfo">Save Chats</span><Link to="/team/invite" className="zmdi zmdi-file zmdi-hc-2x"></Link></div>
+								
+							</div>
+						</div>
 					</div>
 
 						<ul className="activeTeamMember">
