0d7b8f3d5292b475d38e43c1f227706ad9cfd656
root <root@reylink.com>
2016-03-05 20:02:09 -0500

    Fixing webrtc bugs

--- core/app/components/video.jsx
+++ core/app/components/video.jsx
@@ -1,4 +1,4 @@
-import WebRtc from 'react-simplewebrtc';
+import WebRtc from './react_simplewebrtc';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
@@ -6,21 +6,36 @@ class VideoBox extends Component {
 
     constructor(props) {
         super(props);
-     }
+
+        
+    }
+    
+    componentWillReceiveProps(props)
+    {
+
+    }
 
     render() {
-       var infoObject = {
-         roomname : this.props.activeTeam ? this.props.activeTeam._id : "lobby",
-         /*provide a valid url for signalmaster otherwise this won't work*/
-        signalmasterUrl : "reylink.com:8888"
-      };
+    
+    
+      
+    
+         var infoObject = {
+           roomname : this.props.activeTeam ? this.props.activeTeam._id : "lobby",
+           /*provide a valid url for signalmaster otherwise this won't work*/
+           signalmasterUrl : "reylink.com:8888"
+         };
 
-    return (
-          <div className="videoBox">
-               <WebRtc options = {infoObject} />
-          </div>
-           );
-    }
+        return ( <div className="videoBox">
+               <WebRtc roomname={this.props.activeTeam ? this.props.activeTeam._id : "lobby"} options = {infoObject} />
+          </div>  );
+           
+
+ 
+   }   
+    
+
+ 
  }
 
  function mapStateToProps(state) {
