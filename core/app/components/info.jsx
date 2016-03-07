ad074dc9e0150238c81000ccdc41b2ebf5e4639e
ReyPena <zonickblack@gmail.com>
2016-03-07 11:16:26 -0700

    Finish information screen, and chat.

---
+++ core/app/components/info.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+export default class Info extends Component {
+  render() {
+    return (
+      <div className="align">
+        <div className="info">
+          <h2 className="infoTitle">Welcome!</h2>
+          <p>Getting started is easy. Just follow these simple instructions:</p>
+          <ol className="infoList">
+            <li>To create a new team, click <i className="zmdi zmdi-plus-circle-o infoButton"></i></li>
+            <li>Enter a name of the new team you want to create</li>
+            <li>Click <button className="pure-button pure-button-primary">Create</button>!</li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
