1fd1d0264521f760d6727c54b7c5a54c8f44a97d
Mark Gillis <viscid@gmail.com>
2016-03-08 09:48:15 -0700

    Changes alien gravatar to mystery man

--- core/app/components/avatar.jsx
+++ core/app/components/avatar.jsx
@@ -3,7 +3,7 @@ import Gravatar from  "gravatar";
 
 class Avatar extends Component {
   render() {
-    const avatar = Gravatar.url(this.props.email, {s: "50", r: "pg", d: "monsterid"});
+    const avatar = Gravatar.url(this.props.email, {s: "50", r: "pg", d: "mm"});
     return(
       <div className="gravatarIcon">
         <img src={avatar} alt="avatar"></img>
